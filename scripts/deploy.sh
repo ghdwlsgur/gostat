6cda3c635bfc76c000859a58e4b8053cb7af4d42
HongJinHyeok <redmax45@naver.com>
2023-05-11 10:46:11 +0900

    remove legacy code

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash 
 set -e -o pipefail
 
+
+
 trap '[ "$?" -eq 0 ] || echo "Error Line:<$LINENO> Error Function:<${FUNCNAME}>"' EXIT
 cd "$(dirname "$0")" && cd ..
 CURRENT=$(pwd)
@@ -31,7 +33,7 @@ function release
   git tag -a "$tag" -m "Add $tag"
   git push origin "$tag"
 
-  goreleaser release --rm-dist
+  goreleaser release --clean
 }
 
 function release_test
@@ -39,7 +41,7 @@ function release_test
   sudo rm -rf "$CURRENT"/dist "$CURRENT"/gopath  
   export GOPATH="$CURRENT"/gopath
 
-  goreleaser release --snapshot --rm-dist
+  goreleaser release --snapshot --clean
 }
 
 CMD=$1
